4365730ba44596015e1b7ce1429feeaf2cef092c
ammabel95-create <ammabel95@gmail.com>
2025-11-28 09:08:51 +0700

    Initial commit

---
+++ admin/footer.php
@@ -0,0 +1,62 @@
+<footer class="bg-dark text-light pt-5 pb-4 mt-4">
+  <div class="container text-center text-md-start">
+    <div class="row">
+
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase fw-bold mb-4 text-info">Layanan</h6>
+        <p><a href="#" class="text-light text-decoration-none footer-link">Cuci Kering</a></p>
+        <p><a href="#" class="text-light text-decoration-none footer-link">Cuci Setrika</a></p>
+        <p><a href="#" class="text-light text-decoration-none footer-link">Setrika Saja</a></p>
+        <p><a href="#" class="text-light text-decoration-none footer-link">Dry Cleaning</a></p>
+      </div>
+
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase fw-bold mb-4 text-info">Paket</h6>
+        <p><a href="#" class="text-light text-decoration-none footer-link">Harian</a></p>
+        <p><a href="#" class="text-light text-decoration-none footer-link">Mingguan</a></p>
+        <p><a href="#" class="text-light text-decoration-none footer-link">Bulanan</a></p> 
+        <p><a href="#" class="text-light text-decoration-none footer-link">Express</a></p>
+      </div>
+
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase fw-bold mb-4 text-info">Kontak</h6>
+        <p><i class="fas fa-map-marker-alt me-2 text-warning"></i>Jl. Melati No. 123, Jakarta</p>
+        <p><i class="fas fa-envelope me-2 text-warning"></i>info@laundryku.com</p>
+        <p><i class="fas fa-phone me-2 text-warning"></i>+62 812 3456 7890</p>
+      </div>
+
+      <div class="col-md-3 col-lg-4 col-xl-4 mx-auto mt-3">
+        <h6 class="text-uppercase fw-bold mb-4 text-info">Ikuti Kami</h6>
+        <a href="#" class="btn btn-outline-light btn-floating m-1 rounded-circle"><i class="fab fa-facebook-f"></i></a>
+        <a href="#" class="btn btn-outline-light btn-floating m-1 rounded-circle"><i class="fab fa-instagram"></i></a>
+        <a href="#" class="btn btn-outline-light btn-floating m-1 rounded-circle"><i class="fab fa-whatsapp"></i></a>
+
+        <h6 class="mt-4 text-info">Berlangganan</h6>
+        <form class="d-flex">
+          <input type="email" class="form-control me-2" placeholder="Email Anda">
+          <button class="btn btn-info fw-bold text-dark">Subscribe</button>
+        </form>
+      </div>
+    </div>
+
+    <hr class="my-4 bg-light">
+
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <p class="mb-0">&copy; 2025 <span class="text-info fw-bold">LaundryKu</span> | All Rights Reserved</p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<style>
+  .footer-link:hover {
+    color: #0dcaf0 !important; /* warna biru info */
+    margin-left: 5px;
+    transition: all 0.3s;
+  }
+</style>
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
